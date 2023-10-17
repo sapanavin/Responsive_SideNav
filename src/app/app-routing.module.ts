48e415fa67558f4073e3fefb426605ba04026917
sapanavin <116043155+sapanavin@users.noreply.github.com>
2023-10-17 16:01:26 -0500

    Responsice sidenav Part 1 completed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProductsComponent } from './products/products.component';
+import { StatisticsComponent } from './statistics/statistics.component';
+import { CouponsComponent } from './coupons/coupons.component';
+import { PagesComponent } from './pages/pages.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+ 
+  {path: 'dashboard', component:DashboardComponent},
+  {path: 'products', component:ProductsComponent},
+  {path: 'statistics', component:StatisticsComponent},
+  {path: 'coupons', component:CouponsComponent},
+  {path: 'pages', component:PagesComponent},
+  {path: 'settings', component:DashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+
+interface SideNavToggle {
+  screenWidth: number;
+  collapsed: boolean;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +13,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Responsive_Sidenav';
+  isSideNavCollapsed = false;
+  screenWidth = 0;
+
+  onToggleSidenNav(data: SideNavToggle): void{
+    this.screenWidth = data.screenWidth;
+    this.isSideNavCollapsed = data.collapsed;
+    console.log('this.isSideNavCollapsed',this.isSideNavCollapsed);
+    console.log('this.screenWidth',this.screenWidth);
+
+  }
 }
